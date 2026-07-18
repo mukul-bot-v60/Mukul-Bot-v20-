6025e9412bcdfa1f6c4133b64082ead588d150d3
mukul-bot-v60 <mdmukul11708@gmail.com>
2026-07-18 19:45:56 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+const config = require("./config.json");
+const Main = require("./Main");
+
+console.clear();
+
+console.log("================================");
+console.log(`🤖 ${config.botName}`);
+console.log(`👤 Owner : ${config.owner}`);
+console.log(`🔰 Prefix: ${config.prefix}`);
+console.log("================================");
+
+const bot = new Main(config);
+
+bot.start();
