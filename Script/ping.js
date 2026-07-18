8a6f9267e4ffffb940ef560b93802e043a0e462e
mukul-bot-v60 <mdmukul11708@gmail.com>
2026-07-18 20:26:59 +0600

    Update ping.js

--- Script/ping.js
+++ Script/ping.js
@@ -1,1 +1,19 @@
 
+module.exports.config = {
+  name: "ping",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "Mukul",
+  description: "Check bot response",
+  commandCategory: "System",
+  usages: "",
+  cooldowns: 5
+};
+
+module.exports.run = async function ({ api, event }) {
+  return api.sendMessage(
+    "🏓 Pong!",
+    event.threadID,
+    event.messageID
+  );
+};
