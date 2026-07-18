ca0f62df979d75507b7a91303ee6e8731df4a41d
mukul-bot-v60 <mdmukul11708@gmail.com>
2026-07-18 20:30:46 +0600

    Update help.js

--- Script/help.js
+++ Script/help.js
@@ -1,1 +1,14 @@
+module.exports = {
+  name: "help",
+  description: "Show all available commands",
 
+  run({ commands, reply }) {
+    let message = "📖 Available Commands:\n\n";
+
+    commands.forEach(cmd => {
+      message += `• ${cmd.name} - ${cmd.description}\n`;
+    });
+
+    reply(message);
+  }
+};
